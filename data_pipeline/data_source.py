2a8f399f2b3e223a7c62590713e285a8dcc529e4
lester <lesterso1027@gmail.com>
2022-05-14 14:56:30 +0800

    data_source - Add the concrete class WikiDataSource

--- data_pipeline/data_source.py
+++ data_pipeline/data_source.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-from data_pipeline.data_loader import FMPDataLoader, WGBDataLoader
+from data_pipeline.data_loader import FMPDataLoader, WGBDataLoader, WikiDataLoader
 
 class DataSource(ABC):
 
@@ -22,3 +22,10 @@ class WGBDataSource(DataSource):
 
     def create_loader(self):
         return WGBDataLoader(self.source_url)
+
+class WikiDataSource(DataSource):
+
+    source_url = 'https://en.wikipedia.org/wiki/List_of_countries_by_real_GDP_growth_rate'
+
+    def create_loader(self):
+        return WikiDataLoader(self.source_url)
