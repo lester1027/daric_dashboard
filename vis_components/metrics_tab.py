0d14fb9dc7843c1598894310ce726c14e7c58dd8
lester <lesterso1027@gmail.com>
2022-08-24 21:51:37 +0800

    metrics_tab - Remove testing dataframe

--- vis_components/metrics_tab.py
+++ vis_components/metrics_tab.py
@@ -8,7 +8,6 @@ from main_dash import app
 tab_metrics_layout = html.Div(
     children=[
         dcc.Interval(id='metrics-start', interval=1, max_intervals=1),
-        dash_table.DataTable(id={'type': 'trial', 'detail': 'MSFT'}),
         html.H2('Metrics'),
         html.Hr(),
         html.Div(
