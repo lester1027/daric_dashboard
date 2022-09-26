be3af15e307ce28e24b611b5cc1854bdfe36c542
lester <lesterso1027@gmail.com>
2022-09-26 23:25:32 +0800

    main_dash - Add __name__ to the Dash init

--- main_dash.py
+++ main_dash.py
@@ -1,4 +1,4 @@
 from dash_extensions.enrich import Output, DashProxy, Input, MultiplexerTransform, html
 
-app = DashProxy(transforms=[MultiplexerTransform()])
+app = DashProxy(__name__, transforms=[MultiplexerTransform()])
 app.config.suppress_callback_exceptions = True
