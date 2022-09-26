5ab86b4a44ab8447819300803571bb94435b9dd8
lester <lesterso1027@gmail.com>
2022-09-26 23:53:53 +0800

    main_dash - Add the Google AdSense script to the index string

--- main_dash.py
+++ main_dash.py
@@ -2,3 +2,23 @@ from dash_extensions.enrich import Output, DashProxy, Input, MultiplexerTransfor
 
 app = DashProxy(__name__, transforms=[MultiplexerTransform()])
 app.config.suppress_callback_exceptions = True
+app.index_string = '''
+<!DOCTYPE html>
+<html>
+    <head>
+        {%favicon%}
+        {%css%}
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6437726536363317"
+            crossorigin="anonymous">
+        </script>
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>
+'''
